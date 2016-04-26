0b11172117fad2056effbbe13c971d19359a454a
Long Hoang <longly@seespace.co>
2016-04-26 18:22:04 +0700

    fix webhook

--- src/routes/webhook_route.js
+++ src/routes/webhook_route.js
@@ -5,11 +5,16 @@ module.exports = [{
     description: 'webhook for facebook',
     handler: {
       async: async function(req, reply) {
-        if (req.payload('hub.verify_token') === 'QAoi9ft8PgQn(A') {
-          return reply(req.payload('hub.challenge'));
-        }
+        try {
+          if (req.query('hub.verify_token') === 'QAoi9ft8PgQn(A') {
+            return reply(req.query('hub.challenge'));
+          }
 
-        return reply('Error, wrong token');
+          return reply('Error, wrong token');
+        } catch (e) {
+          console.log(e.stack);
+          return reply(e.message);
+        }
       }
     }
   }

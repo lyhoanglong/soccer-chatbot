5aef6c2c0c5e36a6935001b226f3705eb9b7edde
Long Hoang <longly@seespace.co>
2016-04-26 18:57:44 +0700

    add response webhook to user

--- src/routes/webhook_route.js
+++ src/routes/webhook_route.js
@@ -1,3 +1,5 @@
+import * as axios from 'axios';
+
 module.exports = [{
   method: 'GET',
   path: '/webhook/',
@@ -19,4 +21,40 @@ module.exports = [{
       }
     }
   }
-}]
+}, {
+  method: 'POST',
+  path: '/webhook/',
+  config: {
+    description: 'response to client',
+    handler: {
+      async: async function(req, reply) {
+        try {
+          console.log(req.payload);
+          let message_events = req.payload.entry[0].messaging;
+
+          for (let event of message_events) {
+            let sender = event.sender.id;
+            if (event.message && event.message.text) {
+              axios.post('https://graph.facebook.com/v2.6/me/messages?access_token=EAAHAXf0aWBoBADOs3U7KHRoBdbfQMC2UXzI9Fh4B1g9nUqfALe51awauK9ZAnuYOYU8cMWke6cZAVZCi1VnXdIhVlZAG0A2BIxTBKjAtyM0fnwRQo26bZAi9X7HCqXGkBwHo0mfHXyWcHL8gMJLJBUmkv314ZBI3GgZAH3xdtbTiAZDZD', {
+                recipient: {
+                  id: sender
+                },
+                message: {
+                  text: 'You said: ' + event.message.text
+                }
+              }).then(response => {
+                console.log(response);
+              }).catch(response => {
+                console.log(response);
+              });
+            }
+          }
+          return reply().code(200);
+        } catch (e) {
+          console.log(e.stack);
+          return reply(e.message);
+        }
+      }
+    }
+  }
+}];
